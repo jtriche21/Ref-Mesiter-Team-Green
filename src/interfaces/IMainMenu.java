f3951d738ca779d67ca406e8994ecead55c29d5d
Cameron Cagle <Cagle@152.30.101.1>
2018-04-20 11:29:44 -0400

    updated library menu interface and class

--- src/interfaces/IMainMenu.java
+++ src/interfaces/IMainMenu.java
@@ -5,19 +5,22 @@ import views.MainMenuCLInput;
 /**
  * 
  * @author Cagle
- *
+ * A interface that gets the type of menu used for the program
  */
 public interface IMainMenu {
-	
+	/** makes a CLI and GUI menu types */
 	public static int MENU_TYPE_CLI = 111, MENU_TYPE_GUI = 112;
-	
+	/** gets the menu choice */
 	public int getChoice();
-	
-	
-	public static IMainMenu getMenu(int menu_type) {
-		if(menu_type == MENU_TYPE_CLI)
+	/**
+	 * gets the type of menu
+	 * @param menutype
+	 * @return MainMenuCLInput: a command line input
+	 */
+	public static IMainMenu getMenu(int menutype) {
+		if(menutype == MENU_TYPE_CLI)
 			return new MainMenuCLInput();
 		return null;
 		
-	}
-}
+	}// end getMenu
+}// end interface
